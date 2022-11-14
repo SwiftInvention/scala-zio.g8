07b24cb03f3e9e91f8e622f0e79722698389f0ec
Vladimir Logachev <vladimir@logachev.dev>
2022-11-14 15:45:14 +0400

    Use templating for displaying the project name

--- src/main/g8/$name;format="normalize"$/src/main/scala/$package$/Application.scala
+++ src/main/g8/$name;format="normalize"$/src/main/scala/$package$/Application.scala
@@ -3,7 +3,7 @@ package $package$
 object Application {
   def main(args: Array[String]): Unit = {
     println("-" * 50)
-    println("scala-zio template")
+    println("$name$")
     println("-" * 50)
   }
 }
