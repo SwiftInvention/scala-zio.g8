f7ed29d2356e63695714ab2465c0421f9bb02263
akolebakin <akolebakin@swiftinvention.com>
2022-11-15 00:49:46 +0400

    add migration file

--- src/main/g8/$name;format="normalize"$/src/main/resources/db/migration/V1_0__init_schema.sql
+++ src/main/g8/$name;format="normalize"$/src/main/resources/db/migration/V1_0__init_schema.sql
@@ -1,1 +1,11 @@
--- your first db migration file
+CREATE TABLE IF NOT EXISTS person
+(
+    name      varchar(255),
+    birthDate TIMESTAMP NOT NULL,
+    PRIMARY KEY (name)
+);
+
+INSERT IGNORE INTO person(name, birthDate)
+VALUES ('Martin Odersky', '1970-05-19 01:42:01');
+INSERT IGNORE INTO person(name, birthDate)
+VALUES ('James Gosling', '1970-09-05 01:42:01');
