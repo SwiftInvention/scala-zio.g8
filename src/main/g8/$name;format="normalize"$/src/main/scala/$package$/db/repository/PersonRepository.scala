e5e03c60fddada5d4fba4da6681d4f546d696bdd
akolebakin <akolebakin@swiftinvention.com>
2022-11-18 18:50:58 +0400

    edit schema for PersonRepository

--- src/main/g8/$name;format="normalize"$/src/main/scala/$package$/db/repository/PersonRepository.scala
+++ src/main/g8/$name;format="normalize"$/src/main/scala/$package$/db/repository/PersonRepository.scala
@@ -1,16 +1,11 @@
 package $package$.db.repository
 
-import $package$.db.DbContext.ctx
-import $package$.db.model.Person
+import $package.db.DbContext.ctx._
+import $package.db.DbContext._
+import $package.db.model.Person
 import zio.{Task, ZIO}
 
 trait PersonRepository {
-  import ctx._
-
-  val persons = quote {
-    querySchema[Person]("person", _.name -> "name", _.birthDate -> "birth_date")
-  }
-
   def getAllPersons: Task[List[Person]] = ZIO.attempt {
     val q = ctx.quote {
       persons
