1438c59a05d8124a68158b803f735e2300ef1e26
Vladimir Logachev <vladimir@logachev.dev>
2022-11-25 02:25:51 +0400

    Add request timeout middleware

--- src/main/g8/$name;format="normalize"$/src/main/scala/$package$/Application.scala
+++ src/main/g8/$name;format="normalize"$/src/main/scala/$package$/Application.scala
@@ -8,16 +8,30 @@ import zhttp.http.HttpApp
 import zhttp.service.{EventLoopGroup, Server}
 import zhttp.service.server.ServerChannelFactory
 import zio._
+import zhttp.http._
+import zio.clock.Clock
+import java.time.Duration
 
 object Application extends App {
-  val app: HttpApp[Any, Throwable] = ZioHttpInterpreter().toHttp(common)
+  // FIXME: for an unknown reason, the specified duration of 10 seconds simply doesn't work,
+  // And the mysql connection timeout is used instead.
+  private lazy val composedMiddlewares = Middleware.timeout(Duration.ofSeconds(10))
+
+  private lazy val app: HttpApp[Has[Clock.Service], Throwable] =
+    ZioHttpInterpreter().toHttp(SwaggerApiEndpoint.common) @@ composedMiddlewares
 
   def run(args: List[String]) =
     (for {
-      _ <- Migration.migrate
+      _    <- Migration.migrate
       conf <- ZIO.service[HttpServerConfig]
+      // FIXME: Doesn't compile with the dead code compiler warning turned on
       _ <- Server(app).withBinding(conf.host, conf.port).start
     } yield ())
-      .provideLayer(HttpServerConfig.layer ++ EventLoopGroup.auto(0) ++ ServerChannelFactory.auto)
+      .provideLayer(
+        HttpServerConfig.layer
+          ++ EventLoopGroup.auto(0)
+          ++ ServerChannelFactory.auto
+          ++ Clock.live
+      )
       .exitCode
 }
